869efda6976d76445aad13000e0020e1b7dec275
anwargates <102929799+anwargates@users.noreply.github.com>
2023-02-05 22:42:37 +0700

    minor update
    - changed user field
    - updated CORS policy

--- config/middlewares.js
+++ config/middlewares.js
@@ -14,7 +14,15 @@ module.exports = [
       },
     },
   },
-  'strapi::cors',
+  // 'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      headers: '*',
+      origin: ['http://localhost:3000', '*']
+    }
+  },
   'strapi::poweredBy',
   'strapi::logger',
   'strapi::query',
