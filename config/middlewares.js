bc460c4472d7b1941a2ae294a2cf77be51d22d65
anwargates <102929799+anwargates@users.noreply.github.com>
2023-02-06 13:24:32 +0700

    Update middlewares.js

--- config/middlewares.js
+++ config/middlewares.js
@@ -14,15 +14,15 @@ module.exports = [
       },
     },
   },
-  // 'strapi::cors',
-  {
-    name: "strapi::cors",
-    config: {
-      enabled: true,
-      headers: "*",
-      origin: ["https://ruang-edit.netlify.app/", "http://localhost:3000/"],
-    },
-  },
+  'strapi::cors',
+  // {
+  //   name: "strapi::cors",
+  //   config: {
+  //     enabled: true,
+  //     headers: "*",
+  //     origin: ["https://ruang-edit.netlify.app/", "http://localhost:3000/"],
+  //   },
+  // },
   "strapi::poweredBy",
   "strapi::logger",
   "strapi::query",
